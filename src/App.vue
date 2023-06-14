425a578a401c635f3d8c5a6cb683d1fc5b91fea7
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-06-14 21:55:58 +0630

    update actions I& improvements

--- src/App.vue
+++ src/App.vue
@@ -13,9 +13,16 @@
         <div>
             <template v-for="expense in expenses" :key="expense.id">
               <div class="flex mb-4 items-center"  >
-                  <p class="w-full " :class="[expense.paid ? 'text-green-500 line-through' : 'text-grey-800']">{{expense.title}} - <span :class="[expense.paid ? 'text-green-500' : 'text-red-500']">{{expense.amount}} MMK</span></p>
-                  <button class="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded  text-grey border-grey  hover:text-white" @click="togglePaid(expense)" :class="[expense.paid ? 'text-grey border-grey hover:bg-gray-500' : 'text-green-500 border-green-500 hover:bg-green-500']">{{expense.paid ? 'Unpaid' : 'Paid'}}</button>
-                   <button  @click="remove(expense)" class="flex-no-shrink p-2 ml-2 border-2 rounded text-red-500 border-red-500 hover:text-white hover:bg-red-500">Remove</button>
+                  <div v-if="expense.isEdit" class="flex mt-4">
+                    <input ref="expenseInput" v-model="expense.title" class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker" placeholder="Expense Name">
+                    <input v-model="expense.amount" type="number" class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker" placeholder="Amount">
+                     <button  @click="updateExpense(expense)" class="flex-no-shrink p-2 ml-2 border-2 rounded text-blue-500 border-blue-500 hover:text-white hover:bg-blue-500">Update</button>
+                  </div>
+                  <template  v-else>
+                    <p class="w-full " :class="[expense.paid ? 'text-green-500 line-through' : 'text-grey-800']"  @dblclick="editExpense(expense)">{{expense.title}} - <span :class="[expense.paid ? 'text-green-500' : 'text-red-500']">{{expense.amount}} MMK</span></p>
+                    <button class="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded  text-grey border-grey  hover:text-white" @click="togglePaid(expense)" :class="[expense.paid ? 'text-grey border-grey hover:bg-gray-500' : 'text-green-500 border-green-500 hover:bg-green-500']">{{expense.paid ? 'Unpaid' : 'Paid'}}</button>
+                     <button  @click="remove(expense)" class="flex-no-shrink p-2 ml-2 border-2 rounded text-red-500 border-red-500 hover:text-white hover:bg-red-500">Remove</button>
+                  </template>
               </div>
             </template>
         </div>
@@ -38,6 +45,10 @@ export default {
     }
   },
   methods :{
+    editExpense(expense) {
+      let updateExpense = this.expenses.find(e => e.id === expense.id);
+      updateExpense.isEdit = true;
+    },
     addExpense() {
       let data = {title:this.title, paid : false, amount :this.amount, date : serverTimestamp()};
       let ref = collection(db,'expenses');
@@ -68,7 +79,7 @@ export default {
       onSnapshot(q, (snapshot) => {
         this.expenses = [];
         snapshot.docs.forEach(doc => {
-          this.expenses.push({id : doc.id ,...doc.data()})
+          this.expenses.push({id : doc.id ,...doc.data(), isEdit : false})
       })
     });
       
@@ -82,7 +93,7 @@ export default {
     },
     totalLeftAmount() {
         return this.expenses.reduce((total, expense) => {
-          if (!expense.paid) {
+          if (!expense.paid && !expense.isEdit) {
             return Number(total) + Number(expense.amount);
           }
           return total;
